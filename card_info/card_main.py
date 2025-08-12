93ffc71256694e512a3a99a1d99c5832b2739f09
stepanguranov <stepan.guryanov@glowbyteconsulting.com>
2025-08-12 11:49:43 +0300

    доделал mvp

--- card_info/card_main.py
+++ card_info/card_main.py
@@ -5,10 +5,8 @@ from dateutil.relativedelta import relativedelta
 from datetime import datetime, timedelta
 import time
 import calendar
-import re
 import numpy as np
 from sqlalchemy import create_engine
-from pandas.tseries.offsets import MonthEnd
 
 
 def with_retry_on_connection_error(fn: Callable[..., Any], retries: int = 5, delay: float = 3.0) -> Callable:
@@ -74,23 +72,16 @@ def create_conn_dwh():
 
 def convert_dtypes(df):
     dtype_dict = {
-        'date_start': 'datetime64[ns]',
-        'date_end': 'datetime64[ns]',
+        'date': 'datetime64[ns]',
+        'pan': 'object',
         'currency': 'object',
-        'payment_system': 'object',
-        'bank_name': 'object',
-        'bank_country': 'object',
-        'cluster': 'object',
-        'engine': 'object',
-        'client_name': 'object',
         'orders_count': 'int64',
         'success_orders_count': 'int64',
-        'user_count': 'int64',
         'amount_sum': 'float64',
         'success_amount_sum': 'float64',
         'reject_amount_sum': 'float64',
         'avg_close_time': 'float64',
-        'granularity': 'object'
+        'order_type': 'object'
     }
     for col, dtype in dtype_dict.items():
         if col in df.columns:
@@ -171,11 +162,9 @@ def get_invoice_data_by_days(start_date: str, end_date: str, type_order: str):
             print(f"День {day_counter}: {current_date.strftime('%Y-%m-%d')}")
             print(type_order)
             if type_order == 'invoice':
-                print('q')
                 query_get_data = query_get_data_inplace(day_start, day_end)
             else:
                 query_get_data = query_get_data_payout(day_start, day_end, clients_ids=tuple([3960, 3949]))
-            print(query_get_data)
             try:
                 daily_data = run_query_with_conn(conn, query_get_data)
                 if not daily_data.empty:
@@ -198,32 +187,8 @@ def get_invoice_data_by_days(start_date: str, end_date: str, type_order: str):
     return final_df
 
 
-# def parce_device(user_agent):
-#     if not isinstance(user_agent, str):
-#         return 'Unknown'
-#     match = re.search(r'\(([^;]+)', user_agent)
-#     if match:
-#         first_part = match.group(1).strip()
-#         first_word = first_part.split()[0]
-#         return first_word
-#     return 'Unknown'
-
-
-def assign_cluster(count):
-    match count:
-        case 1:
-            return '1'
-        case 2:
-            return '2'
-        case n if 3 <= n <= 5:
-            return '3 - 5'
-        case n if 5 < n <= 10:
-            return '6 - 10'
-        case n if n > 10:
-            return '10+'
-
-
 def agg_data(date_start: str, date_end: str, order_type: str) -> pd.DataFrame:
+    print('launch')
     data = get_invoice_data_by_days(date_start, date_end, order_type)
 
     data['created_at'] = pd.to_datetime(data['created_at'], utc=True, errors='coerce')
@@ -242,11 +207,14 @@ def agg_data(date_start: str, date_end: str, order_type: str) -> pd.DataFrame:
     data['success_amount'] = data['amount'].where(data['status_id'] == success_status_id, 0)
     data['reject_amount'] = data['amount'].where(data['status_id'] != success_status_id, 0)
     data_group = data.copy()
-
+    print(data_group.shape, 'qaz')
     for col in data_group.select_dtypes(include=['datetimetz']).columns:
         data_group[col] = data_group[col].dt.tz_localize(None)
 
     data_group['date'] = data_group['created_at'].dt.to_period('D')
+    print(data_group)
+    print(data_group.columns)
+    data_group.to_csv('data_group.csv', index=False)
     data_group = (
         data_group
         .groupby(
@@ -261,6 +229,7 @@ def agg_data(date_start: str, date_end: str, order_type: str) -> pd.DataFrame:
         )
         .reset_index()
     )
+    print(data_group.shape, 'qaz')
     data_group['date'] = data_group['date'].astype(str)
     return data_group
 
@@ -284,15 +253,14 @@ def execute_functions_mode(mode, order_type):
     if mode == 'update':
         conn = create_conn_dwh()
         date_end = \
-            run_query_dwh(f"""SELECT max(date_end) as max_date FROM cascade.e_come_payments_summary
-                                     """, conn)['max_date'].iloc[0]
+            run_query_dwh(f"""SELECT max("date") as max_date FROM cascade.cards_info_sg""", conn)['max_date'].iloc[0]
 
         date_start = (date_end - relativedelta(months=1)).strftime('%Y-%m-%d')
         date_end = date_end.strftime('%Y-%m-%d')
         print(date_end, date_start)
         data_invoice = agg_data(date_start, date_end, order_type)
-        query_get_from_dwh = f"""SELECT * FROM cascade.e_come_payments_summary
-                                 WHERE "date_start" >= '{date_start}' AND "date_end" <= '{date_end}'
+        query_get_from_dwh = f"""SELECT * FROM cascade.cards_info_sg
+                                 WHERE "date" >= '{date_start}' AND "date" <= '{date_end}'
                              """
         conn = create_conn_dwh()
         data_from_dwh = run_query_dwh(query_get_from_dwh, conn)
@@ -306,17 +274,13 @@ def execute_functions_mode(mode, order_type):
         data_invoice = data_invoice.round(2)
         data_from_dwh = data_from_dwh.round(2)
 
-        print(data_invoice.dtypes)
-        print(data_from_dwh.dtypes)
-
         data_invoice = data_invoice.merge(data_from_dwh,
-                                          on=['date', 'pan'],
+                                          on=['date', 'pan', 'currency'],
                                           how='inner', suffixes=('_new', '_old'))
         data_invoice.to_csv('test_inv.csv', index=False)
         data_invoice = data_invoice[(data_invoice['amount_sum_new'] != data_invoice['amount_sum_old']) |
                                     (data_invoice['success_orders_count_new'] != data_invoice[
                                         'success_orders_count_old']) |
-                                    (data_invoice['user_count_new'] != data_invoice['user_count_old']) |
                                     (data_invoice['success_amount_sum_new'] != data_invoice['success_amount_sum_old']) |
                                     (data_invoice['reject_amount_sum_new'] != data_invoice['reject_amount_sum_old']) |
                                     (data_invoice['avg_close_time_new'] != data_invoice['avg_close_time_old'])
@@ -327,52 +291,36 @@ def execute_functions_mode(mode, order_type):
             columns={col: col.replace('_new', '') for col in data_invoice.columns if col.endswith('_new')})
 
         conn.close()
-        # conn = create_conn_dwh()
-        # print(data_invoice)
-        # with conn.cursor() as cursor:
-        #     for index, row in data_invoice.iterrows():
-        #         update_query = """
-        #         UPDATE cascade.e_come_payments_summary
-        #         SET amount_sum = %s,
-        #             success_orders_count = %s,
-        #             user_count = %s,
-        #             success_amount_sum = %s,
-        #             reject_amount_sum = %s,
-        #             avg_close_time = %s  -- передаем интервал
-        #         WHERE date_start = %s AND
-        #               date_end = %s AND
-        #               currency = %s AND
-        #               payment_system = %s AND
-        #               bank_name = %s AND
-        #               bank_country = %s AND
-        #               cluster = %s AND
-        #               granularity = %s AND
-        #               order_type = %s AND
-        #               engine = %s AND
-        #               client_name = %s
-        #         """
-        #         cursor.execute(update_query, (
-        #             row['amount_sum'],
-        #             row['success_orders_count'],
-        #             row['user_count'],
-        #             row['success_amount_sum'],
-        #             row['reject_amount_sum'],
-        #             row['avg_close_time'],
-        #             row['date_start'],
-        #             row['date_end'],
-        #             row['currency'],
-        #             row['payment_system'],
-        #             row['bank_name'],
-        #             row['bank_country'],
-        #             row['cluster'],
-        #             row['granularity'],
-        #             order_type,
-        #             row['engine'],
-        #             row['client_name']
-        #         ))
-        #     conn.commit()
-        #     conn.close()
-        # print(data_invoice.shape)
+        conn = create_conn_dwh()
+        print(data_invoice)
+        with conn.cursor() as cursor:
+            for index, row in data_invoice.iterrows():
+                update_query = """
+                UPDATE cascade.cards_info_sg
+                SET amount_sum = %s,
+                    success_orders_count = %s,
+                    success_amount_sum = %s,
+                    reject_amount_sum = %s,
+                    avg_close_time = %s  
+                WHERE "date" = %s AND
+                      pan = %s AND
+                      currency = %s AND
+                      order_type = %s      
+                """
+                cursor.execute(update_query, (
+                    row['amount_sum'],
+                    row['success_orders_count'],
+                    row['success_amount_sum'],
+                    row['reject_amount_sum'],
+                    row['avg_close_time'],
+                    row['date'],
+                    row['pan'],
+                    row['currency'],
+                    order_type
+                ))
+            conn.commit()
+            conn.close()
+        print(data_invoice.shape)
         return data_invoice
     else:
         now = datetime.now()
@@ -381,11 +329,13 @@ def execute_functions_mode(mode, order_type):
 
         date_start = date_start.strftime('%Y-%m-%d')
         date_end = date_end.strftime('%Y-%m-%d')
-        date_start = '2025-08-11'
+        date_start = '2025-08-10'
         date_end = '2025-08-11'
         print(date_start, date_end)
         data_invoice = agg_data(date_start, date_end, order_type)
         data_invoice['order_type'] = order_type
+    print(data_invoice.dtypes)
+    print(data_invoice.shape)
     if not data_invoice.empty:
         data_invoice = data_invoice.round(2)
         data_invoice.to_csv('result.csv', index=False)
@@ -409,19 +359,19 @@ def execute_functions_mode(mode, order_type):
     return data_invoice
 
 
-def main(granularity):
+def main():
     execute_functions_mode(mode='upload', order_type='payout')
-    print(f'Отработал: mode - upload, granularity - {granularity}, order_type - payout')
+    print('Отработал: mode - upload, order_type - payout')
+
+    # execute_functions_mode(mode='update', order_type='payout')
+    # print('Отработал: mode - update, order_type - payout')
     #
-    # execute_functions_mode(mode='update', granularity=granularity, order_type='payout')
-    # print(f'Отработал: mode - update, granularity - {granularity}, order_type - payout')
-
-    execute_functions_mode(mode='upload', order_type='invoice')
-    print(f'Отработал: mode - upload, granularity - {granularity}, order_type - invoice')
-
-    # execute_functions_mode(mode='update', granularity=granularity, order_type='invoice')
-    # print(f'Отработал: mode - update, granularity - {granularity}, order_type - invoice')
+    # execute_functions_mode(mode='upload', order_type='invoice')
+    # print('Отработал: mode - upload, order_type - invoice')
+    #
+    # execute_functions_mode(mode='update', order_type='invoice')
+    # print('Отработал: mode - update, order_type - invoice')
 
 
 if __name__ == '__main__':
-    main(granularity='D')
+    main()
